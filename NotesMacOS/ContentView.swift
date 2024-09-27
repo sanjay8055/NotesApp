8e3dddba49391e67fb5d12904860ddc86fcf978d
Sanjay Raskar <sanjayraskars@gmail.com>
2024-09-27 16:25:51 +0530

    Initial Commit

---
+++ NotesMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesMacOS
+//
+//  Created by Sanjay Raskar on 27/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesMacOS/NotesMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesMacOSApp.swift
+//  NotesMacOS
+//
+//  Created by Sanjay Raskar on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
